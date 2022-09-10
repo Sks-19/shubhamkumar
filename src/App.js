75c6c3f42369d4695178a59da020b2843d4e8d0a
beingsksharma <30936556+beingsksharma@users.noreply.github.com>
2022-09-10 20:14:14 +0530

    project added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import Card from './card';
-import Footer from './footer';
-import Resume from './Resume';
-import Work from './work';
-import ContactMe from './Contact';
-import Navbar from './navbar';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.min.js";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Card from "./card";
+import Footer from "./footer";
+import Resume from "./Resume";
+import Work from "./work";
+import ContactMe from "./Contact";
+import Navbar from "./navbar";
 
 function App() {
   return (
--- src/card.js
+++ src/card.js
@@ -7,60 +7,62 @@ import "./card.css";
 function Card() {
   return (
     <>
-      <div className="row justify-content-center">
-        <div className="col-sm-12 col-md-6 col-lg-6">
-          <div className="our-team">
-            <div className="picture">
-              <img className="img-fluid" src={image} alt="Shubham Kumar" />
-            </div>
-            <div className="team-content">
-              <h2 className="name">
-                <span>S</span>hubham <span>K</span>umar
-              </h2>
-              <hr className="Hr" />
-              <h4 className="title">
-                <span>W</span>EB <span>D</span>EVELOPER
-              </h4>
-            </div>
-            <NavLink
-              to="/resume"
-              type="button"
-              className="btn btn-primary resumeSubmit"
-            >
-              RESUME
-            </NavLink>
-            <NavLink
-              to="/work"
-              type="button"
-              className="btn btn-primary projectSubmit"
-            >
-              PROJECTS
-            </NavLink>
-          </div>
-        </div>
-
-        <div className="col-sm-12 col-md-6 col-lg-6">
-          <div className="About">
-            <h1 className="Hello">
-              About <span style={{ fontWeight: "bold" }}>Me</span>
-            </h1>
-            <br />
-            <p>
-              I'm a Software Engineer with 1.8 years of experience in Web
-              Development, with proficiency in web technologies like React,
-              JavaScript, HTML5, CSS3, and Bootstrap, building large-scale web
-              applications, cross-browser platforms, and design constraints on
-              the web, and building responsive user interfaces.
-            </p>
-            <div className="downloadCV">
-              <a
-                href="Shubham Kumar.pdf"
-                download="shubham kumar.pdf"
+      <div className="container">
+        <div className="row">
+          <div className="col-sm-12 col-md-6 col-lg-6">
+            <div className="our-team">
+              <div className="picture">
+                <img className="img-fluid" src={image} alt="Shubham Kumar" />
+              </div>
+              <div className="team-content">
+                <h2 className="name">
+                  <span>S</span>hubham <span>K</span>umar
+                </h2>
+                <hr className="Hr" />
+                <h4 className="title">
+                  <span>W</span>EB <span>D</span>EVELOPER
+                </h4>
+              </div>
+              <NavLink
+                to="/resume"
                 type="button"
-                className="btn btn-primary submit"
+                className="btn btn-primary resumeSubmit"
               >
-                DOWNLOAD CV
-              </a>
+                RESUME
+              </NavLink>
+              <NavLink
+                to="/work"
+                type="button"
+                className="btn btn-primary projectSubmit"
+              >
+                PROJECTS
+              </NavLink>
+            </div>
+          </div>
+
+          <div className="col-sm-12 col-md-6 col-lg-6">
+            <div className="About">
+              <h1 className="Hello">
+                About <span style={{ fontWeight: "bold" }}>Me</span>
+              </h1>
+              <br />
+              <p>
+                I'm a Software Engineer with 1.8 years of experience in Web
+                Development, with proficiency in web technologies like React,
+                JavaScript, HTML5, CSS3, and Bootstrap, building large-scale web
+                applications, cross-browser platforms, and design constraints on
+                the web, and building responsive user interfaces.
+              </p>
+              <div className="downloadCV">
+                <a
+                  href="Shubham Kumar.pdf"
+                  download="shubham kumar.pdf"
+                  type="button"
+                  className="btn btn-primary submit"
+                >
+                  DOWNLOAD CV
+                </a>
+              </div>
             </div>
           </div>
         </div>
